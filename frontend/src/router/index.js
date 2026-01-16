e0dc3260b6ee645aa0a6d598de7759a2dd073ed4
FAL-Black <1658190436@qq.com>
2026-01-16 16:30:49 +0800

    Fix: add missing Register import in router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 import CreatePost from '../views/CreatePost.vue';
 import PostDetail from '../views/PostDetail.vue';
 
